fb5770ab46f1444e0d8067c3d7733c98ad19d051
iceone <oleg.ledvanov@yandex.ru>
2018-04-09 05:33:50 +0300

    example

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,3 +1,9 @@
-$(document).ready(function(){
-  $('.slider').slick();
+$(document).ready(function () {
+  $(document).on('click', 'li:not(.active)', function (e) {
+    $.each(e.target.classList,function(i,v){ //получаем массив классов выбранного элемента
+      if(/tab\d/.test(v)){                  //проверка на наличие класса tab1,2,3, т.к. может быть много классов в любом порядке
+        $('.'+v).addClass('active').siblings().removeClass('active');
+      }
+    });
+  });
 });
